378a6e5eeb3f3dfd361ccfd4280a670690ca335e
ArmandoKoffi <armandokoffikoffi@gmail.com>
2025-10-17 16:50:34 +0000

    Correction de la page blanche d'accueil

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { authService } from '@/services/mockAuth';
+import { authService } from '@/services/api';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Heart, Activity, BarChart3, Users } from 'lucide-react';
